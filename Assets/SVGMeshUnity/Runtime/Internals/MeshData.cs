d3a4fc6bffd470987a8d2236f0f484cfc6962bff
Yoshihiro Shindo <beinteractive.org@gmail.com>
2018-02-27 11:16:30 +0900

    Refactor

---
+++ Assets/SVGMeshUnity/Runtime/Internals/MeshData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVGMeshUnity.Internals
+{
+    public class MeshData
+    {
+        public MeshData()
+        {
+            Vertices = new List<Vector3>();
+            Edges = new List<int>();
+            Triangles = new List<int>();
+        }
+        
+        public List<Vector3> Vertices { get; private set; }
+        public List<int> Edges { get; private set; }
+        public List<int> Triangles { get; private set; }
+
+        public void Clear()
+        {
+            Vertices.Clear();
+            Edges.Clear();
+            Triangles.Clear();
+        }
+
+        public void AddVertices(WorkBuffer<Vector2> buffer)
+        {
+            var firstEdgeIdx = -1;
+
+            for (var i = 0; i < buffer.UsedSize; ++i)
+            {
+                var v = buffer.Data[i];
+                var idx = Vertices.IndexOf(v);
+                if (idx == -1)
+                {
+                    Vertices.Add(v);
+                    idx = Vertices.Count - 1;
+                }
+                
+                Edges.Add(idx);
+
+                if (i == 0)
+                {
+                    firstEdgeIdx = idx;
+                }
+            }
+            
+            Edges.Add(firstEdgeIdx);
+        }
+
+        public void Upload(Mesh m)
+        {
+            m.SetVertices(Vertices);
+            m.SetTriangles(Triangles, 0);
+            m.RecalculateBounds();
+            m.RecalculateNormals();
+        }
+    }
+}
---
+++ Assets/SVGMeshUnity/Runtime/Internals/WorkBuffer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SVGMeshUnity.Internals
+{
+    public class WorkBuffer<T>
+    {
+        public WorkBuffer(int capacity)
+        {
+            Data = new List<T>();
+            Data.AddRange(Enumerable.Repeat(default(T), capacity));
+        }
+        
+        public List<T> Data { get; private set; }
+        public int UsedSize { get; private set; }
+
+        public void Push(ref T val)
+        {
+            if (Data.Count == UsedSize)
+            {
+                Data.Add(val);
+            }
+            else
+            {
+                Data[UsedSize] = val;
+            }
+
+            ++UsedSize;
+        }
+
+        public void Clear()
+        {
+            UsedSize = 0;
+        }
+    }
+}
